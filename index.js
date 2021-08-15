0376ed8e48ae2dea70f3de6ec4eb86c2bc1ade24
Aniket Prakash <b218004@iiit-bh.ac.in>
2021-08-15 09:19:37 +0530

    feat: add css styling

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ function calculateProfitLoss(initial,current,quantity){
         var loss = (initial - current)*quantity;
         var lossPercent = (loss/initial)*100;
         output.innerText = `Sorry to say that you have faced a loss of ${loss} and the loss percent is ${lossPercent}.`;
+        output.style.color = "red";
+        document.body.style.backgroundImage = 'url("https://media.giphy.com/media/3osxYzIQRqN4DOEddC/source.gif")';
     }else if (current > initial) {
         var profit = (current - initial) * quantity;
         var profitPercent = (profit/initial)*100;
-        output.innerText = `Hey you have earned a profit of ${profit} and the profit percent is ${profitPercent}.`;
+        output.innerText = `Hey you have earned a profit of ${profit} and the profit percent is ${profitPercent}%.`;
+        output.style.color = "green";
+        document.body.style.backgroundImage = "url('https://media.giphy.com/media/aTg2U8r9YCgfOlGh45/source.gif')";
     }else {
         output.innerText = `Have some patience DUDE!!`;
     }
